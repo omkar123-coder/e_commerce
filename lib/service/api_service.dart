ef3b6376d75979c31d9f61554cce8609f1f2be4e
Omkar Londhe <omkarlondhe.appdid@gmail.com>
2026-05-25 14:46:10 +0530

    going on

--- lib/service/api_service.dart
+++ lib/service/api_service.dart
@@ -1,17 +1,23 @@
 import 'dart:convert';
+import 'dart:math';
 import 'package:e_commerce/models/category_model.dart';
 import 'package:http/http.dart' as http;
 
 class ApiService {
   static String url = 'https://fakestoreapi.com/products';
   static Future<List<CategoryModel>> fetchProduct() async {
-    final response = await http.get(Uri.parse(url));
-
-    if (response.statusCode == 200) {
-      List data = jsonDecode(response.body);
-      return data.map((e) => CategoryModel.fromJson(e)).toList();
-    } else {
-      throw Exception('Failed to fetch Products');
+    try {
+      final response = await http.get(Uri.parse(url));
+      if (response.statusCode == 200) {
+        List data = jsonDecode(response.body);
+        log(response.toString());
+        return data.map((e) => CategoryModel.fromJson(e)).toList();
+      } else {
+        throw Exception('Failed to fetch Products');
+      }
+    } catch (e) {
+      log('Network error');
+      throw Exception('failed to fetch products');
     }
   }
 }
